a01040fa4e93a052b08808b835e256dd32fe6cd7
jnf03 <85895563+jnf03@users.noreply.github.com>
2021-08-18 01:26:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".share").addEventListener("click", function () {
+  document.querySelector(".active").classList.toggle("visible");
+  document.querySelector(".share").classList.toggle("change");
+});
